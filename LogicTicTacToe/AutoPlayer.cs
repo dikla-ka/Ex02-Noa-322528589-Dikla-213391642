d9ce26b94bb43bf0719e7f4943163268cc842094
karte <diklaka@mta.ac.il>
2026-05-20 14:48:58 +0300

    14:48

--- LogicTicTacToe/AutoPlayer.cs
+++ LogicTicTacToe/AutoPlayer.cs
@@ -7,19 +7,14 @@ using System.Threading.Tasks;
 namespace LogicTicTacToe
 {
     public class AutoPlayer : Player
-    {    
-        //public void Play(Board i_Board)
-        //{
-        //    Random random = new Random();
-        //    int row, col;
-        //    do
-        //    {
-        //        row = random.Next(0, i_Board.GetSize());
-        //        col = random.Next(0, i_Board.GetSize());
-        //    }
-        //    while (!i_Board.IsCellEmpty(row, col));
-    
-        //    i_Board.SetCell(row, col, this.GetSymbol());
-        //}
+    {
+        public override void ChooseCoordinates(out int o_Row, out int o_Col)
+        {
+            o_Row = -1;
+            o_Col = -1;
+            Random random = new Random();
+            //o_Row = random.Next(0, m_Board.GetSize());
+            //o_Col = random.Next(0, m_Board.GetSize());
+        }
     }
 }
---
+++ NotTicTacToe/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotTicTacToe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Choose board size (Between 3 - 9):");
+            while (!ValidateBoardSize())
+            {
+                Console.WriteLine("Invalid board size. Please try again.");
+                Console.WriteLine("Choose board size (Between 3 - 9):");
+            }
+
+            Console.WriteLine("Please choose game mode:");
+            Console.WriteLine("1. Single Player (vs. Computer)"); 
+            Console.WriteLine("2. Two Players (vs. Human)");
+            while (!ValidateGameMode())
+            {
+                Console.WriteLine("Invalid game mode. Please try again.");
+                Console.WriteLine("1. Single Player (vs. Computer)");
+                Console.WriteLine("2. Two Players (vs. Human)");
+            }
+
+
+        }
+
+        public static bool ValidateBoardSize()
+        {
+            bool validInput = int.TryParse(Console.ReadLine(), out int boardSize);
+            return (validInput && boardSize >= 3 && boardSize <= 9);
+        }
+
+        public static bool ValidateGameMode()
+        {
+            bool validInput = int.TryParse(Console.ReadLine(), out int gameMode);
+            return (validInput && (gameMode == 1 || gameMode == 2));
+        }
+    }
+}
