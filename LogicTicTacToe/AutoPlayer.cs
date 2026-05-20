9c6d3040afffbc2ffe484f2be4bc15d6d679696a
karte <diklaka@mta.ac.il>
2026-05-20 13:27:54 +0300

    Play

---
+++ LogicTicTacToe/AutoPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTicTacToe
+{
+    public class AutoPlayer : Player
+    {    
+        //public void Play(Board i_Board)
+        //{
+        //    Random random = new Random();
+        //    int row, col;
+        //    do
+        //    {
+        //        row = random.Next(0, i_Board.GetSize());
+        //        col = random.Next(0, i_Board.GetSize());
+        //    }
+        //    while (!i_Board.IsCellEmpty(row, col));
+    
+        //    i_Board.SetCell(row, col, this.GetSymbol());
+        //}
+    }
+}
---
+++ LogicTicTacToe/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicTicTacToe
+{
+    public class Player
+    {
+        private int m_Id;
+        private int m_Score = 0;
+        private Symbols m_Symbol;
+        private static int m_IdGenerator = 0;
+        
+        public enum Symbols
+        {
+            X = 'X',
+            O = 'O'
+        }
+
+        public Player()
+        {
+            this.m_Id = m_IdGenerator;
+            this.m_Symbol = (Symbols)(m_IdGenerator % 2 == 0 ? 'X' : 'O');
+            m_IdGenerator++;
+        }
+
+        public int GetId()
+        {
+            return m_Id;
+        }
+
+        public Symbols GetSymbol()
+        {
+            return m_Symbol;
+        }
+    }
+}
